585f23249761b75a4b5e4027271d79186fcfdd58
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-05-11 18:45:13 -0300

    chore: first setup

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vitest/config";
+import { resolve } from "node:path";
+
+export default defineConfig({
+  test: {
+    alias: [
+      {
+        find: "~",
+        replacement: resolve(__dirname, "src"),
+      },
+    ],
+  },
+});
