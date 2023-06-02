5f67744e72833874bc8902f71417ba9a66d807b6
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-06-02 20:42:33 -0300

    fix(test): add test to client factory

---
+++ src/test/factory/ClientFactory.spec.ts
@@ -0,0 +1,18 @@
+import { describe, it, expect } from "vitest";
+import { ClientFactory } from "./ClientFactory";
+import { Client } from "@modules/client/domain/client/client";
+
+describe("ClientFactory.ts", () => {
+  it("should create an instance of Client", () => {
+    const result = ClientFactory.create();
+
+    expect(result).toBeInstanceOf(Client);
+  });
+
+  it("should create 3 instances of Client", () => {
+    const clients = ClientFactory.createSome(3);
+
+    expect(clients.length).toBe(3);
+    expect(clients.every((client) => client instanceof Client)).toBeTruthy();
+  });
+});
